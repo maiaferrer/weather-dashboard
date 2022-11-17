1d253ba82e654f6acf7e33fd73c5c3ea3225f557
Maia Ferrer <maiaferrer@Maias-MacBook-Air.local>
2022-11-17 15:02:45 -0500

    added pseudo code and code files

---
+++ script.js
@@ -0,0 +1,28 @@
+// TOP OF PAGE
+// create an array for recent searches
+
+// create vars for apikey, apiurl
+
+// create cars for html elements
+
+
+
+// BODY OF PAGE
+// create function to get items from local storage, then put into recent searches array, then render
+// call dayjs
+
+// function to get data from search bar { get value from search bar, to be used in fetch request }
+
+// function to fetch main weather data 
+// transform data
+// get icon value from api --> if statement to find out type of weather -->
+// assign data to var or create and append to page 
+
+
+
+// AT BOTTOM OF PAGE
+// run function to get recent searches 
+
+// create button to run function when user inputs destination
+// add event listener for recent searches
+
